371a806925761e2d38ecb91989474f7417a1aa41
Estevam <estevamfurtado@gmail.com>
2022-07-26 13:51:18 -0300

    new db

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://repoprovas-bootcamp.herokuapp.com/",
 });
 
 interface UserData {
